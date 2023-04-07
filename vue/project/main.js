0e9627d11636b4dd8ad5e883931f97119a175ece
MiyoshiRyou <code34434@gmail.com>
2023-04-07 15:19:20 +0900

    computed property

--- vue/project/main.js
+++ vue/project/main.js
@@ -6,6 +6,18 @@ new Vue({
       body: 'bodyです',
       url: 'https://google.com',
       message: '',
+      text1: '',
+      text2: '',
+    }
+  },
+  computed: {
+    capitalizedText1() {
+      console.log('computed');
+      if (!this.text1) {
+        return '';
+      }
+
+      return this.text1.charAt(0).toUpperCase() + this.text1.slice(1);
     }
   },
   methods: {
@@ -17,6 +29,14 @@ new Vue({
     },
     inputValue(event) {
       this.message = event.target.value;
+    },
+    capitalizedText2() {
+      console.log('method');
+      if (!this.text2) {
+        return '';
+      }
+
+      return this.text2.charAt(0).toUpperCase() + this.text2.slice(1);
     }
   }
 })
