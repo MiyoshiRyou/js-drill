4ec4eeafe6e9d250d394881187c8b6800343af82
MiyoshiRyou <code34434@gmail.com>
2023-04-13 00:15:24 +0900

    button component

--- vue/with-vue-cli/my-app/src/App.vue
+++ vue/with-vue-cli/my-app/src/App.vue
@@ -1,10 +1,22 @@
 <template>
   <div id="app">
     <h1>Hello</h1>
+    <Button />
+    <Button />
+    <Button />
   </div>
 </template>
 
 <script>
+import Button from './components/Button';
+
+export default {
+  name: 'Fuga',
+  components: {
+    Button
+  }
+}
+
 </script>
 
 <style>
