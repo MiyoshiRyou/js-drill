1ff5da6d564872cd33d8ba4689bd86a80886db99
MiyoshiRyou <code34434@gmail.com>
2023-04-15 18:54:23 +0900

    props

--- vue/with-vue-cli/my-app/src/App.vue
+++ vue/with-vue-cli/my-app/src/App.vue
@@ -1,19 +1,27 @@
 <template>
   <div id="app">
     <h1>Hello</h1>
-    <Button />
-    <Button />
-    <Button />
+    <Counter
+      button-title="ボタン1"
+      :initial-count="-10"
+    />
+    <Counter
+      buttonTitle="ボタン2"
+      :initialCount="100"
+    />
+    <Counter
+      buttonTitle="ボタン3"
+    />
   </div>
 </template>
 
 <script>
-import Button from './components/Button';
+import Counter from './components/Counter';
 
 export default {
   name: 'Fuga',
   components: {
-    Button
+    Counter
   }
 }
 
